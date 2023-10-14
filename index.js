a959bf4ccd3ca62df2c64ad27cf76a265baa8367
VaishMax5 <96114037+VaishMax5@users.noreply.github.com>
2023-10-14 11:37:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+//console.log("TOday is fri");
+
+//document.write("<br> <h1> JS is a PL")//used to print something in body
+
+// let() can be reassigned but cannot be redeclared
+//let num1=22;
+//let num1=23(not possible)
+//num1=23;
+
+//var can be reassigned and redeclared also
+
+//var num2=40;
+//var num2=42;//(can be done)
+//var2=43;//(can be done)
+
+//constant cammot be redeclared or reassigned
+
+//const num3=88;
+//console.log(num3);
+
+//object
+
+//let person={
+  //  name:"<NAME>",
+   // mob:974366628,
+    //gender:"Male",
+//};
+//document.write(person.name);
+
+//array
+
+//let vehicle=["car", "bike"];
+//console.log(vehicle[0]);
+//console.log(vehicle.length);
+
+//function
+//function addNum(num1,num2) {
+  //  return (num1+num2);}
+    //document.write(addNum(20,30));
+
+
+//let age=16;
+//if(age>=18) {
+  //  document.write("Elligible for vote");
+//} else {
+  //  document.write("Not Elligible for vote");
+//}
+
+//.innerHTML will give the value of the id
+
+//document.getElementById
+//document.getElementsByClassName
+//document.querySelector()
+//document.querySelectorAll()
+//document.getElementsByTagName
+
+//let el = document.getElementById("head")
+//console.log(el);
+//output:
+//<h1 id="head">
+//Hi Everyone
+//</h1>
+
+//let el = document.getElementById("head").innerHTML
+//console.log(el);
+//ans- Hi Everyone
